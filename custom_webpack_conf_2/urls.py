c337d390fc4b3951fdf2b30eaae425d20a39fc38
valentinogagliardi <valentino@valentinog.com>
2019-04-24 19:59:01 +0200

    Move REST API urls in project settings

--- custom_webpack_conf_2/urls.py
+++ custom_webpack_conf_2/urls.py
@@ -1,7 +1,3 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView
-)
 from django.urls import path
 from .views import index, LinkList
 
@@ -10,6 +6,4 @@ PREFIX = 'api'
 urlpatterns = [
     path('link2/', index),
     path(f'{PREFIX}/link2/', LinkList.as_view()),
-    path(f'{PREFIX}/user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path(f'{PREFIX}/user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
--- django_rest_react/urls.py
+++ django_rest_react/urls.py
@@ -1,22 +1,16 @@
-"""django_rest_react URL Configuration
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
+PREFIX = 'api'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(f'{PREFIX}/user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(f'{PREFIX}/user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('custom_webpack_conf_2.urls'))
 ]
