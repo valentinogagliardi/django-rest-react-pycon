fe48ab3c59f023a4e97c7ef147046dd8b0fd387b
valentinogagliardi <valentino@valentinog.com>
2019-04-24 21:58:33 +0200

    Enable CORS

--- django_rest_react/settings/base.py
+++ django_rest_react/settings/base.py
@@ -18,12 +18,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'custom_webpack_conf_2'
+    'custom_webpack_conf_2',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- django_rest_react/settings/dev.py
+++ django_rest_react/settings/dev.py
@@ -1,3 +1,8 @@
 from .base import *
 
 DEBUG = True
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+    '127.0.0.1:3000'
+)
